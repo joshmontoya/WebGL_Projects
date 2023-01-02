f25526b2742cf367db95e2c0db574b2502e100b4
Josh Montoya <84519241+joshmontoya@users.noreply.github.com>
2023-01-01 19:59:40 -0700

    Update mod2meth2.js

--- CSC405_CT/Module_2/Method_2/mod2meth2.js
+++ CSC405_CT/Module_2/Method_2/mod2meth2.js
@@ -5,7 +5,10 @@ var gl = canvas.getContext('experimental-webgl');
 
 /* Define initial triangle geometry and store it in buffer objects */
 
-var vertices = [-0.5, -0.5, 0.0, 0.5, 0.5, -0.5];
+var a = vec2(-0.5, -0.5);
+var b = vec2(0.0, 0.5);
+var c = vec2(0.5, -0.5);
+var vertices = [a[0], a[1], b[0], b[1], c[0], c[1]];
 
 // Create a new buffer object
 var vertex_buffer = gl.createBuffer();
@@ -36,7 +39,7 @@ gl.shaderSource(vertShader, vertCode);
 gl.compileShader(vertShader);
 
 // Fragment shader source code
-var fragCode = 'void main(void) {' + 'gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);' + '}';
+var fragCode = 'void main(void) {' + 'gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);' + '}';
 
 // Create fragment shader object
 var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
