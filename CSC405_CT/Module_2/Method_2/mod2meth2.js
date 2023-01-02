195431c7a4eaf421be83ab9fffe106bd3d924924
Josh Montoya <84519241+joshmontoya@users.noreply.github.com>
2023-01-01 18:48:57 -0700

    Update mod2meth2.js

--- CSC405_CT/Module_2/Method_2/mod2meth2.js
+++ CSC405_CT/Module_2/Method_2/mod2meth2.js
@@ -17,91 +17,184 @@ var gl = canvas.getContext('experimental-webgl');
 
 var vertices = [-0.5, -0.5, 0.0, 0.5, 0.5, -0.5];
 
-// // Create a new buffer object
-// var vertex_buffer = gl.createBuffer();
+// Create a new buffer object
+var vertex_buffer = gl.createBuffer();
 
-// // Bind an empty array buffer to it
-// gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+// Bind an empty array buffer to it
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
 
-// // Pass the vertices data to the buffer
-// gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+// Pass the vertices data to the buffer
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-// // Unbind the buffer
-// gl.bindBuffer(gl.ARRAY_BUFFER, null);
+// Unbind the buffer
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-// /* Create and compile Shader programs for initial triangle */
+/* Create and compile Shader programs for initial triangle */
 
-// // Vertex shader source code
-// var vertCode = 
-//   'attribute vec2 coordinates;' + 
-//   'void main(void) {' + ' gl_Position = vec4(coordinates, 0.0, 1.0);' + '}';
+// Vertex shader source code
+var vertCode = 
+  'attribute vec2 coordinates;' + 
+  'void main(void) {' + ' gl_Position = vec4(coordinates, 0.0, 1.0);' + '}';
 
-// // Create a vertex shader object
-// var vertShader = gl.createShader(gl.VERTEX_SHADER);
+// Create a vertex shader object
+var vertShader = gl.createShader(gl.VERTEX_SHADER);
 
-// // Attach vertex shader source code
-// gl.shaderSource(vertShader, vertCode);
+// Attach vertex shader source code
+gl.shaderSource(vertShader, vertCode);
 
-// // Compile the vertex shader
-// gl.compileShader(vertShader);
+// Compile the vertex shader
+gl.compileShader(vertShader);
 
-// // Fragment shader source code
-// var fragCode = 'void main(void) {' + 'gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);' + '}';
+// Fragment shader source code
+var fragCode = 'void main(void) {' + 'gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);' + '}';
 
-// // Create fragment shader object
-// var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+// Create fragment shader object
+var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
 
-// // Attach fragment shader source code
-// gl.shaderSource(fragShader, fragCode);
+// Attach fragment shader source code
+gl.shaderSource(fragShader, fragCode);
 
-// // Compile the fragment shader
-// gl.compileShader(fragShader);
+// Compile the fragment shader
+gl.compileShader(fragShader);
 
-// // Create a shader program object to store combined shader program
-// var shaderProgram = gl.createProgram();
+// Create a shader program object to store combined shader program
+var shaderProgram = gl.createProgram();
 
-// // Attach a vertex shader
-// gl.attachShader(shaderProgram, vertShader);
+// Attach a vertex shader
+gl.attachShader(shaderProgram, vertShader);
 
-// // Attach a fragment shader
-// gl.attachShader(shaderProgram, fragShader);
+// Attach a fragment shader
+gl.attachShader(shaderProgram, fragShader);
 
-// // Link both programs
-// gl.linkProgram(shaderProgram);
+// Link both programs
+gl.linkProgram(shaderProgram);
 
-// // Use the combined shader program object
-// gl.useProgram(shaderProgram);
+// Use the combined shader program object
+gl.useProgram(shaderProgram);
 
-// /* Associate the shader programs to buffer objects */
+/* Associate the shader programs to buffer objects */
 
-// // Bind vertex buffer object
-// gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+// Bind vertex buffer object
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
 
-// // Get the attribute location
-// var coord = gl.getAttribLocation(shaderProgram, "coordinates");
+// Get the attribute location
+var coord = gl.getAttribLocation(shaderProgram, "coordinates");
 
-// // Point an attribute to the currently bound VBO
-// gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
+// Point an attribute to the currently bound VBO
+gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
 
-// // Enable the attribute
-// gl.enableVertexAttribArray(coord);
+// Enable the attribute
+gl.enableVertexAttribArray(coord);
 
-// /* Draw the initial triangle */
+/* Draw the initial triangle */
 
-// // Clear the canvas
-// gl.clearColor(0.0, 0.0, 0.0, 0.1);
+// Clear the canvas
+gl.clearColor(0.0, 0.0, 0.0, 0.1);
 
-// // Enable the depth test
-// gl.enable(gl.DEPTH_TEST);
+// Enable the depth test
+gl.enable(gl.DEPTH_TEST);
 
-// // Clear the color buffer bit
-// gl.clear(gl.COLOR_BUFFER_BIT);
+// Clear the color buffer bit
+gl.clear(gl.COLOR_BUFFER_BIT);
 
-// // Set the view port
-// gl.viewport(0, 0, canvas.width, canvas.height);
+// Set the view port
+gl.viewport(0, 0, canvas.width, canvas.height);
 
-// // Draw the triangle
-// gl.drawArrays(gl.TRIANGLES, 0, 3);
+// Draw the triangle
+gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+//FIXME
+
+  var vertices_dup = [-0.5, -0.5, -0.25, -0.25, 0, -0.25];
+ 
+  // Create a new buffer object
+  var vertex_buffer2 = gl.createBuffer();
+
+  // Bind an empty array buffer to it
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer2);
+
+  // Pass the vertices data to the buffer
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices_dup), gl.STATIC_DRAW);
+
+  // Unbind the buffer
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  /* Create and compile Shader programs for initial triangle */
+
+  // Vertex shader source code
+  var vertCode2 = 
+    'attribute vec2 coordinates;' + 
+    'void main(void) {' + ' gl_Position = vec4(coordinates, 0.0, 1.0);' + '}';
+
+  // Create a vertex shader object
+  var vertShader2 = gl.createShader(gl.VERTEX_SHADER);
+
+  // Attach vertex shader source code
+  gl.shaderSource(vertShader2, vertCode2);
+
+  // Compile the vertex shader
+  gl.compileShader(vertShader2);
+
+  // Fragment shader source code
+  var fragCode2 = 'void main(void) {' + 'gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);' + '}';
+
+  // Create fragment shader object
+  var fragShader2 = gl.createShader(gl.FRAGMENT_SHADER);
+
+  // Attach fragment shader source code
+  gl.shaderSource(fragShader2, fragCode2);
+
+  // Compile the fragment shader
+  gl.compileShader(fragShader2);
+
+  // Create a shader program object to store combined shader program
+  var shaderProgram2 = gl.createProgram();
+
+  // Attach a vertex shader
+  gl.attachShader(shaderProgram2, vertShader2);
+
+  // Attach a fragment shader
+  gl.attachShader(shaderProgram2, fragShader2);
+
+  // Link both programs
+  gl.linkProgram(shaderProgram2);
+
+  // Use the combined shader program object
+  gl.useProgram(shaderProgram2);
+
+  /* Associate the shader programs to buffer objects */
+
+  // Bind vertex buffer object
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer2);
+
+  // Get the attribute location
+  var coord2 = gl.getAttribLocation(shaderProgram2, "coordinates");
+
+  // Point an attribute to the currently bound VBO
+  gl.vertexAttribPointer(coord2, 2, gl.FLOAT, false, 0, 0);
+
+  // Enable the attribute
+  gl.enableVertexAttribArray(coord2);
+
+  /* Draw the initial triangle */
+
+  // Clear the canvas
+  gl.clearColor(0.0, 0.0, 0.0, 0.1);
+
+  // Enable the depth test
+  gl.enable(gl.DEPTH_TEST);
+
+  // Clear the color buffer bit
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Set the view port
+  gl.viewport(0, 0, canvas.width, canvas.height);
+
+  // Draw the triangle
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+
+//FIXME
 
 
 
@@ -112,19 +205,19 @@ function sierpinski(a, b, c) {
 //   triangle(a, b, c);
 
   // Compute midpoints of each side
-  const ab = mix(a, b, 0.5);
-  const bc = mix(b, c, 0.5);
-  const ac = mix(a, c, 0.5);
+  var ab = mix(a, b, 0.5);
+  var bc = mix(b, c, 0.5);
+  var ac = mix(a, c, 0.5);
 
   // Add veritices of newly formed triangles based on midpoints into triangleVertices array
   triangle(a, ab, ac);
 //   triangle(ab, b, bc);
 //   triangle(ac, bc, c);
 
-  const vertices_dup = [-0.5, -0.5, -0.25, -0.25, 0, -0.25];  //FIXME
+  var vertices_dup = [-0.5, -0.5, -0.25, -0.25, 0, -0.25];  //FIXME
  
   // Create a new buffer object
-  const vertex_buffer = gl.createBuffer();
+  var vertex_buffer = gl.createBuffer();
 
   // Bind an empty array buffer to it
   gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
@@ -138,12 +231,12 @@ function sierpinski(a, b, c) {
   /* Create and compile Shader programs for initial triangle */
 
   // Vertex shader source code
-  const vertCode = 
+  var vertCode = 
     'attribute vec2 coordinates;' + 
     'void main(void) {' + ' gl_Position = vec4(coordinates, 0.0, 1.0);' + '}';
 
   // Create a vertex shader object
-  const vertShader = gl.createShader(gl.VERTEX_SHADER);
+  var vertShader = gl.createShader(gl.VERTEX_SHADER);
 
   // Attach vertex shader source code
   gl.shaderSource(vertShader, vertCode);
@@ -152,10 +245,10 @@ function sierpinski(a, b, c) {
   gl.compileShader(vertShader);
 
   // Fragment shader source code
-  const fragCode = 'void main(void) {' + 'gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);' + '}';
+  var fragCode = 'void main(void) {' + 'gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);' + '}';
 
   // Create fragment shader object
-  const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+  var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
 
   // Attach fragment shader source code
   gl.shaderSource(fragShader, fragCode);
@@ -164,7 +257,7 @@ function sierpinski(a, b, c) {
   gl.compileShader(fragShader);
 
   // Create a shader program object to store combined shader program
-  const shaderProgram = gl.createProgram();
+  var shaderProgram = gl.createProgram();
 
   // Attach a vertex shader
   gl.attachShader(shaderProgram, vertShader);
@@ -184,7 +277,7 @@ function sierpinski(a, b, c) {
   gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
 
   // Get the attribute location
-  const coord = gl.getAttribLocation(shaderProgram, "coordinates");
+  var coord = gl.getAttribLocation(shaderProgram, "coordinates");
 
   // Point an attribute to the currently bound VBO
   gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
@@ -208,8 +301,6 @@ function sierpinski(a, b, c) {
 
   // Draw the triangle
   gl.drawArrays(gl.TRIANGLES, 0, 3);
- 
-  return;
 
   }
   // Call sierpinski function for first (test) sub-triangle
